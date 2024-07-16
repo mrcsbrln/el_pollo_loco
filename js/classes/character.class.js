e82f020ee6025b6ac01fa1f27e845f1e90b88644
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-07-16 23:48:54 +0200

    update character animation

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     height = 280;
     width = 130;
     y = 155;
+    speed = 10;
     IMAGES_WALKING = [
         '../assets/img/2_character_pepe/2_walk/W-21.png',
         '../assets/img/2_character_pepe/2_walk/W-22.png',
@@ -21,14 +22,24 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
     jump() {
