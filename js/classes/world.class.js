6dc9a953647f6ade3132941e9d89692d043b028d
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-07-18 19:20:30 +0200

    update character animation

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -51,6 +51,16 @@ class World {
     }
 
     addToCanvas(movableObject) {
+        if (movableObject.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(movableObject.width, 0);
+            this.ctx.scale(-1, 1);
+            movableObject.x = movableObject.x * -1
+        }
         this.ctx.drawImage(movableObject.img, movableObject.x,  movableObject.y, movableObject.width, movableObject.height);
+        if (movableObject.otherDirection) {
+            movableObject.x = movableObject.x * -1
+            this.ctx.restore();
+        }
     }
 }
