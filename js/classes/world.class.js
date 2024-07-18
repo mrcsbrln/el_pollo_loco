d22163bdbae89f865b0de10805c6bff838f9b078
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-07-18 21:28:02 +0200

    update background objects

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -9,14 +9,35 @@ class World {
         new Cloud
     ];
     backgroundObjects = [
+        new BackgroundObject('../assets/img/5_background/layers/air.png', -719),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', -719),
+
         new BackgroundObject('../assets/img/5_background/layers/air.png', 0),
         new BackgroundObject('../assets/img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('../assets/img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('../assets/img/5_background/layers/1_first_layer/1.png', 0),
+
+        new BackgroundObject('../assets/img/5_background/layers/air.png', 719),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('../assets/img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/1.png', 719*2),
+
+        new BackgroundObject('../assets/img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', 719*3),        
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -32,10 +53,12 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToCanvas(this.backgroundObjects);
         this.addToCanvas(this.character);
         this.addObjectsToCanvas(this.clouds);
         this.addObjectsToCanvas(this.enemies);
+        this.ctx.translate(-this.camera_x, 0);
 
 
         let self = this;
