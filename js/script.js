06ff1eb73ebed040136b068832be56af327c2992
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-08-15 22:57:37 +0200

    startGame funktion bug fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function startGame() {
     init();
+    document.getElementById('play-btn').disabled = true;
     document.getElementById('intro-outro-screens').style.display = 'none';
     document.getElementById('canvas-div').style.display = 'block';
     document.getElementById('play-btn').style.zIndex = '-1';
 }
+
